a3c064b290d7580cc4a483e3e0fec9079763a757
Jonathunky <12529409+Jonathunky@users.noreply.github.com>
2024-11-13 14:34:13 +0000

    Horsing around

--- app/config.ts
+++ app/config.ts
@@ -1,16 +1,16 @@
 export const metaData = {
-  baseUrl: "https://nextfolio-template.vercel.app/",
-  title: "Nextfolio",
-  name: "Sirius",
+  baseUrl: "https://evgenii.org/",
+  title: "me_irl",
+  name: "Evgenii Ostrovskii",
   ogImage: "/opengraph-image.png",
   description:
     "A clean, fast, and lightweight portfolio template built with Next.js, Vercel, and Tailwind CSS for optimal performance.",
 };
 
 export const socialLinks = {
-  twitter: "https://x.com/1tssirius",
-  github: "https://github.com/1msirius/Nextfolio",
-  instagram: "https://www.instagram.com/",
-  linkedin: "https://www.linkedin.com/",
-  email: "mailto:example@gmail.com",
+  twitter: "https://x.com/JonathanSeriesX",
+  github: "https://github.com/JonathanSeriesX",
+  instagram: "https://www.instagram.com/JonathanSeriesX",
+  linkedin: "https://www.linkedin.com/in/jonathunky",
+  //email: "mailto:example@gmail.com",
 };

cefbb2bfb8f9a3eb9c7b563c4ef127b70415ca01
JonathanSeriesX <12529409+JonathanSeriesX@users.noreply.github.com>
2025-10-15 00:14:59 +0200

    Implemented a nice font

--- config/fonts.ts
+++ config/fonts.ts
@@ -1,8 +1,12 @@
-import { Fira_Code as FontMono, Inter as FontSans } from "next/font/google";
+import { Fira_Code as FontMono } from "next/font/google";
+import localFont from "next/font/local";
 
-export const fontSans = FontSans({
-  subsets: ["latin"],
+export const fontSans = localFont({
+  src: "../public/fonts/TofinoVariable.woff2",
   variable: "--font-sans",
+  display: "swap",
+  weight: "100 800",
+  style: "oblique 0deg 1deg",
 });
 
 export const fontMono = FontMono({
